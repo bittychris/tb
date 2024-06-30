ebbd61c5948d84dd58c108d1202eac8a07a58956
bashirii <bashiriidd3@gmail.com>
2024-06-30 19:42:59 +0300

    Tried query I used on RC dashboard component

--- app/Livewire/AdminPanel/ReportList.php
+++ app/Livewire/AdminPanel/ReportList.php
@@ -395,6 +395,42 @@ class ReportList extends Component
                 ->where('created_by', Auth::user()->id)
                 ->latest()
                 ->paginate(10);
+            // Form::query()
+            // ->when($this->keywords, function ($query) {
+            //     return $query->where(function ($query) {
+            //         $query->where('scanning_name', 'like', '%' . $this->keywords . '%')
+            //             // ->orWhere('created_at', $this->date)
+            //             ->orWhereHas('ward', function ($query) {
+            //                 $query->where('name', 'like', '%' . $this->keywords . '%');
+            //             })
+            //             ->orWhereHas('added_by', function ($query) {
+            //                 $query->where('first_name', 'like', '%' . $this->keywords . '%')
+            //                     ->orWhere('last_name', 'like', '%' . $this->keywords . '%');
+            //             })
+            //             ->orWhereHas('ward.district', function ($query) {
+            //                 $query->where('name', 'like', '%' . $this->keywords . '%');
+            //             })
+            //             ->orWhereHas('ward.district.region', function ($query) {
+            //                 $query->where('name', 'like', '%' . $this->keywords . '%');
+            //             });
+            //     });
+            // })
+            // ->when($this->submission_status, function ($query) {
+
+            //     $query->where('status', 'like', '%' . $this->submission_status . '%');
+            // })
+            // ->when($this->date, function ($query) {
+
+            //     $query->whereBetween('created_at', ['2022-01-07', $this->date]);
+            // })
+            // ->with(['added_by', 'form_attribute', 'ward' => function ($query) {
+            //     $query->with(['district' => function ($district) {
+            //         $district->with('region');
+            //     }]);
+            // }])
+            // ->where('created_by', Auth::user()->id)
+            // ->latest()
+            // ->limit(10)->get();
         }
 
         return view('livewire.admin-panel.report-list', ['reports' => $reports]);
